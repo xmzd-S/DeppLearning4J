addb99805125a853cb6dd3f2788b56dd8032e886
xmzd-S <80734020+xmzd-S@users.noreply.github.com>
2022-07-29 11:53:25 +0800

    Update Demo1.java

--- src/main/java/com/xiumuzidiao/dp/course1Week2/Demo1.java
+++ src/main/java/com/xiumuzidiao/dp/course1Week2/Demo1.java
@@ -5,8 +5,9 @@ import org.nd4j.linalg.factory.Nd4j;
 
 public class Demo1 {
     public static void main(String[] args) {
-        
+        new Demo1().test1();
     }
+
     //Exercise 1  Build a function that returns the sigmoid of a real number x. Use math.exp(x) for the exponential function.
     @Test
     public void test1() {
@@ -25,6 +26,8 @@ public class Demo1 {
     public void test3(){
 
         System.out.println(sigmoidGrad(Nd4j.createFromArray(1.0, 2.0, 3.0)));
+
+
 
     }
 
@@ -57,13 +60,30 @@ public class Demo1 {
         // 按行进行归一化，得到的是系数
         System.out.println(softMax(reshape));
     }
+
+//Implement the numpy vectorized version of the L1 loss. You may find the function abs(x) (absolute value of x) useful.
+    @Test
+    public void test7(){
+        INDArray yHat = Nd4j.createFromArray(.9, 0.2, 0.1, .4, .9);
+        INDArray y = Nd4j.createFromArray(1.0, 0.0 , 0.0 , 1.0 , 1.0 );
+
+        System.out.println(loss(yHat,y));
+    }
+//Implement the numpy vectorized version of the L2 loss
+    @Test
+    public void test8(){
+        INDArray yHat = Nd4j.createFromArray(.9, 0.2, 0.1, .4, .9);
+        INDArray y = Nd4j.createFromArray(1.0, 0.0 , 0.0 , 1.0 , 1.0 );
+
+        System.out.println(loss2(yHat,y));
+    }
     public static double sigmoidOne(double x){
 
         return 1.0/(Math.exp(-x)+1);
     }
 
     public static INDArray sigmoidTwo(INDArray x){
-
+        
         return Nd4j.math.pow(Nd4j.math().exp(x.mul(-1)).add(1),-1.0);
     }
 
@@ -88,6 +108,23 @@ public class Demo1 {
         INDArray broadcast = sum.broadcast(x.shape()[0], x.shape()[1]);
         INDArray resu = x.div(broadcast);
         return resu;
+    }
+    public static INDArray loss(INDArray yHat,INDArray y){
+        
+        INDArray newY = yHat.sub(y);
+        INDArray absolutNewY = Nd4j.math().abs(newY);
+        INDArray sum = absolutNewY.sum(true, 0);
+
+
+        return sum;
+
+    }
+
+    public static INDArray loss2(INDArray yHat,INDArray y){
+        INDArray newY = yHat.sub(y);
+        INDArray absolutNewY = Nd4j.math().pow(newY,2.0);
+        INDArray sum = absolutNewY.sum(true, 0);
+        return sum;
     }
 
 }
